851f62efe7018e3f150ffa31d6e205e544ac90ad
Waigwa Gatamu <mail@waigs.me>
2020-04-15 01:17:25 +0100

    Event clicks

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Button from "@material-ui/core/Button";
 import DownArrow from "@material-ui/icons/KeyboardArrowDownOutlined";
 import { injectIntl } from "react-intl";
@@ -6,16 +6,40 @@ import styles from "./styles";
 import copy from "../../copy.json";
 
 export const DropDown = ({ intl }) => {
+  const node = useRef();
   const classes = styles();
   const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+
+  const handleClick = (e) => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setIsOpen(false);
+  };
+
+  const handleChange = () => setIsOpen(!isOpen);
   const toggleClass = isOpen
     ? `${classes.dropDown} ${classes.dropDownActive}`
     : `${classes.dropDown}`;
 
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className={classes.root}>
-      <Button onClick={toggle} variant="contained" endIcon={<DownArrow />}>
+    <div ref={node} className={classes.root}>
+      <Button
+        onClick={handleChange}
+        variant="contained"
+        endIcon={<DownArrow />}
+      >
         {intl.formatMessage(copy.appBar.button)}
       </Button>
       <div className={`${toggleClass}`}>Show Me</div>
