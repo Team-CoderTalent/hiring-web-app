8acd9695c6fdccd25235311d655e2ce34f3a6be0
Damian <damian.borek@codertalent.io>
2020-04-19 14:50:01 +0100

    Fixed database mock

--- backend/src/__test__/index.spec.ts
+++ backend/src/__test__/index.spec.ts
@@ -1,11 +1,22 @@
 import app from "../index";
 const request = require('supertest');
+import mongoose from 'mongoose';
+import { MockMongoose } from 'mock-mongoose';
 
 const server = request(app);
 const apiPath = "/api/v1";
-
+let mockMongoose: MockMongoose = new MockMongoose(mongoose);
 
 describe("index/init", () => {
+  beforeAll(() => {
+    mockMongoose.prepareStorage().then(() => {
+      mongoose.connect('mongodb://foobar/baz');
+      mongoose.connection.on('connected', () => {
+        console.log('db connection is now open');
+      });
+    });
+  });
+
   it("should initiate the app", () => {
     expect(app).toBeTruthy();
   });
